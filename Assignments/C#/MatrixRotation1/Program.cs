91d5adcfdef69e226dbe907bf5c55320695b702f
Sandipan Ghosh <sandipan@promact.com>
2017-02-15 17:46:39 +0530

    done

---
+++ Assignments/C#/MatrixRotation1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] arr = input.Split(' ');
+            int row = Convert.ToInt32(arr[0]);
+            int col = Convert.ToInt32(arr[1]);
+            int rotations = Convert.ToInt32(arr[2]);
+
+            int[,] matrix = new int[row, col];
+
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+
+            RotateAntiClockwiseByLayer(matrix, rotations);
+
+            for (int i = 0; i < row; i++)
+            {
+                string s = "";
+                for (int j = 0; j < col; j++)
+                    s += matrix[i, j] + " ";
+                Console.WriteLine(s.Trim());
+            }
+            Console.ReadLine();
+        }
+
+
+        public static bool RotateAntiClockwiseByLayer(int[,] matrix, int steps)
+        {
+            if (matrix == null) return false;
+
+            int rows = matrix.GetLength(0);  // row
+            int cols = matrix.GetLength(1);  // column 
+
+            int startRow = 0;
+            int startCol = 0;
+
+            while (rows > 1 && cols > 1)
+            {
+                int circleLength = 2 * rows + 2 * cols - 4;
+                int actualSteps = steps % circleLength;
+
+                for (int step = 0; step < actualSteps; step++) //  move one step anti-clockwise a time
+                {
+                    int lt_corner = matrix[startRow, startCol]; // left-top corner element
+
+                    for (int colIndex = startCol; colIndex < startCol + cols - 1; colIndex++)
+                        matrix[startRow, colIndex] = matrix[startRow, colIndex + 1];
+
+                    int lastCol = startCol + cols - 1;
+                    for (int rowIndex = startRow; rowIndex < startRow + rows - 1; rowIndex++)
+                        matrix[rowIndex, lastCol] = matrix[rowIndex + 1, lastCol];
+
+
+                    int lastRow = startRow + rows - 1;
+                    for (int colIndex = startCol + cols - 1; colIndex >= startCol + 1; colIndex--)
+                        matrix[lastRow, colIndex] = matrix[lastRow, colIndex - 1];
+
+                    for (int rowIndex = startRow + rows - 1; rowIndex >= 1 + startRow; rowIndex--)
+                        matrix[rowIndex, startCol] = matrix[rowIndex - 1, startCol];
+
+                    matrix[startRow + 1, startCol] = lt_corner;
+                }
+
+                startRow++;
+                startCol++;
+
+                rows -= 2;
+                cols -= 2;
+            }
+
+            return true;
+        }
+    }
+}
+
---
+++ Assignments/OOP/Inheritance1/Inheritance1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class A { public void mult(ref int a) { a *= 2; } }
+    class B { public int mult(ref int b) { b *= 3; return b; } }
+    class c { public void mult(ref int c) { c *= 5; } }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 1;
+            int div, c1 = 0, c2 = 0, c3 = 0, initial = 1;
+            A classA = new A();
+            B classB = new B();
+            c classC = new c();
+            Console.WriteLine("Enter New Value");
+            var newvalue = int.Parse(Console.ReadLine());
+            var temp = newvalue;
+            while (true)
+            {
+                if (temp % 2 == 0)
+                {
+                    temp = temp / 2;
+                    classA.mult(ref initial);
+                    c1++;
+                    //Console.WriteLine(div);
+                }
+                else if (temp % 3 == 0)
+                {
+                    temp = temp / 3;
+                    classB.mult(ref initial);
+                    c2++;
+                    //Console.WriteLine(div);
+                }
+                else if (temp % 5 == 0)
+                {
+                    temp = temp / 5;
+                    classC.mult(ref initial);
+                    c3++;
+                    //Console.WriteLine(div);
+
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input");
+                    break;
+                }
+                if (initial == newvalue)
+                {
+                    Console.WriteLine(c1);
+                    Console.WriteLine(c2);
+                    Console.WriteLine(c3);
+                    break;
+                }
+            }
+
+            Console.Read();
+        }
+    }
+
+}
+
